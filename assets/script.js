97f3b6ad5b131032d7660952783917523475cf52
Jamie Thomason <jamie.revill@gmail.com>
2022-07-14 18:00:16 -0400

    adding API key

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
-var inputEl;
-var value = 'london'
-var url = "https://www.google.com/api?search=" + value;
+var nameInputEl = document.querySelector("#city");
+var userFormEl = document.querySelector("#user-form");
+var apiUrl = "http://api.openweathermap.org/" + value;
+var currCityData = document.querySelector("#currcity-container");
+var fiveDayData = document.querySelector("#five-container");
 
 // user types in input
 // click search button
@@ -8,15 +10,80 @@ var url = "https://www.google.com/api?search=" + value;
 // this function grabs the value of the input element;
 // add input value to query url
 // hit fetch request
+//Open weather API key: 077d9b62f9b77d50803e4072a52edcaa
 // wait for response, then dynamically add to screen
 // save search value in localstorage
 
-//Input City --converts to--> Lat/Lon --converts to--> Weather (APIs: coordinates and weather)
+//Input City Name
 
-fetch(url).then(function(res) {
+var formSubmitHandler = function(event) {
+    var cityName = nameInputEl.value.trim();
+    if (cityName) {
+      getCityData(cityName);
+      nameInputEl.value = "";
+    } else {
+      alert("Please enter a city name");
+    }
+};
+
+//Submit Button
+  
+
+//Get Lat/Long API
+
+var getLatLonData = function(user) {
+    "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={077d9b62f9b77d50803e4072a52edcaa}";
+
+fetch(apiUrl).then(function(res) {
     return res.json()
 }).then(function(data) {
     console.log(data);
 }).catch(function(err) {
     console.log(err);
 })
+
+};
+
+//Current Data API
+
+var currCityData= function(user) {
+    "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={A077d9b62f9b77d50803e4072a52edcaa}";
+
+fetch(apiUrl).then(function(res) {
+    return res.json()
+}).then(function(data) {
+    console.log(data);
+}).catch(function(err) {
+    console.log(err);
+})
+
+};
+
+//5-Day Data API
+
+var fiveDayData= function(user) {
+    "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={077d9b62f9b77d50803e4072a52edcaa}";
+
+fetch(apiUrl).then(function(res) {
+    return res.json()
+}).then(function(data) {
+    console.log(data);
+}).catch(function(err) {
+    console.log(err);
+})
+
+};
+
+
+//Load and Save Search Value
+
+var loadCity = function() {
+    cityName = JSON.parse(localStorage.getItem("cityName"));
+};
+
+var saveCity = function() {
+    localStorage.setItem("cityName", JSON.stringify(cityName));
+};
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
